d82ebb511acf57dce176a13e5f1b6c6d62ec03cb
Samadan01 <samadan01@gmail.com>
2023-01-03 10:29:20 +0100

    Addition statement

--- greetings.py
+++ greetings.py
@@ -3,3 +3,8 @@ print('greetings')
 #Show the user some really cool images
 
 #Let the user select for them selves
+
+if you can dream it - you can do it
+
+We have come this far, we can do it together, let's go
+
